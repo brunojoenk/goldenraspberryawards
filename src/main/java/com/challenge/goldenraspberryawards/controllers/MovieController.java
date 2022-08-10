ee9eb1e23670ad78c6bea7c39d7326792ea7a816
Bruno <bjoenk@gmail.com>
2022-08-10 17:43:40 -0300

    Turn async

--- src/main/java/com/challenge/goldenraspberryawards/controllers/MovieController.java
+++ src/main/java/com/challenge/goldenraspberryawards/controllers/MovieController.java
@@ -1,8 +1,8 @@
 package com.challenge.goldenraspberryawards.controllers;
 
 import com.challenge.goldenraspberryawards.dtos.MovieIntervalDTO;
-import com.challenge.goldenraspberryawards.services.MovieIntervalCalculator;
 import com.challenge.goldenraspberryawards.services.MovieInterval;
+import com.challenge.goldenraspberryawards.services.MovieIntervalCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,29 +12,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping(value = "/awards/movies", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MovieController {
 
     private MovieIntervalCalculator movieIntervalCalculator;
+    private MovieInterval movieInterval;
 
     @Autowired
-    public MovieController(final MovieIntervalCalculator movieIntervalCalculator) {
+    public MovieController(final MovieIntervalCalculator movieIntervalCalculator, final MovieInterval movieInterval) {
         this.movieIntervalCalculator = movieIntervalCalculator;
+        this.movieInterval = movieInterval;
     }
 
     @GetMapping("/min-max")
-    public ResponseEntity<MinMaxDTO> get() {
+    public ResponseEntity<MinMaxDTO> get() throws ExecutionException, InterruptedException {
         final List<MovieIntervalDTO> listIntervalOrdered = movieIntervalCalculator.calculateWinnersByIntervalAsc();
 
         if (listIntervalOrdered.size() == 0) {
             return ResponseEntity.ok().body(new MinMaxDTO(Collections.emptyList(), Collections.emptyList()));
         }
 
-        return ResponseEntity.ok().body(new MinMaxDTO(MovieInterval.getMin(listIntervalOrdered), MovieInterval.getMax(listIntervalOrdered)));
+        final CompletableFuture<List<MovieIntervalDTO>> min = movieInterval.getMin(listIntervalOrdered);
+        final CompletableFuture<List<MovieIntervalDTO>> max = movieInterval.getMax(listIntervalOrdered);
+
+        CompletableFuture.allOf(min, max).join();
+
+        return ResponseEntity.ok().body(new MinMaxDTO(min.get(), max.get()));
     }
 
-    record MinMaxDTO(List<MovieIntervalDTO> min, List<MovieIntervalDTO> max) {}
+    record MinMaxDTO(List<MovieIntervalDTO> min, List<MovieIntervalDTO> max) {
+    }
 
 }
--- src/main/java/com/challenge/goldenraspberryawards/services/MovieInterval.java
+++ src/main/java/com/challenge/goldenraspberryawards/services/MovieInterval.java
@@ -1,27 +1,32 @@
 package com.challenge.goldenraspberryawards.services;
 
 import com.challenge.goldenraspberryawards.dtos.MovieIntervalDTO;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+@Component
 public class MovieInterval {
 
-    public static List<MovieIntervalDTO> getMin(final List<MovieIntervalDTO> listIntervalOrdered) {
+    @Async
+    public CompletableFuture<List<MovieIntervalDTO>> getMin(final List<MovieIntervalDTO> listIntervalOrdered) {
         final Integer minInterval = listIntervalOrdered.get(0).getInterval();
 
-        return listIntervalOrdered.stream()
+        return CompletableFuture.completedFuture(listIntervalOrdered.stream()
                 .filter(intervalDTO -> Objects.equals(intervalDTO.getInterval(), minInterval))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
-    public static List<MovieIntervalDTO> getMax(final List<MovieIntervalDTO> listIntervalOrdered) {
+    @Async
+    public CompletableFuture<List<MovieIntervalDTO>> getMax(final List<MovieIntervalDTO> listIntervalOrdered) {
         final Integer maxInterval = listIntervalOrdered.get(listIntervalOrdered.size() - 1).getInterval();
 
-        return listIntervalOrdered.stream()
+        return CompletableFuture.completedFuture(listIntervalOrdered.stream()
                 .filter(intervalDTO -> Objects.equals(intervalDTO.getInterval(), maxInterval))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 }
