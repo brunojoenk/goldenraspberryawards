9f881507e820dd2e35c39777d8f90245be3ea086
Bruno <bjoenk@gmail.com>
2022-08-10 18:07:54 -0300

    added log

--- src/main/java/com/challenge/goldenraspberryawards/services/MovieInterval.java
+++ src/main/java/com/challenge/goldenraspberryawards/services/MovieInterval.java
@@ -1,9 +1,12 @@
 package com.challenge.goldenraspberryawards.services;
 
 import com.challenge.goldenraspberryawards.dtos.MovieIntervalDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -12,13 +15,17 @@ import java.util.stream.Collectors;
 @Component
 public class MovieInterval {
 
+    private static Logger LOG = LoggerFactory.getLogger(MovieInterval.class);
+
     @Async
     public CompletableFuture<List<MovieIntervalDTO>> getMin(final List<MovieIntervalDTO> listIntervalOrdered) {
         final Integer minInterval = listIntervalOrdered.get(0).getInterval();
 
         return CompletableFuture.completedFuture(listIntervalOrdered.stream()
-                .filter(intervalDTO -> Objects.equals(intervalDTO.getInterval(), minInterval))
-                .collect(Collectors.toList()));
+                .filter(intervalDTO -> {
+                    LOG.info("Testing thread async (min)");
+                    return Objects.equals(intervalDTO.getInterval(), minInterval);
+                }).collect(Collectors.toList()));
     }
 
     @Async
@@ -26,7 +33,9 @@ public class MovieInterval {
         final Integer maxInterval = listIntervalOrdered.get(listIntervalOrdered.size() - 1).getInterval();
 
         return CompletableFuture.completedFuture(listIntervalOrdered.stream()
-                .filter(intervalDTO -> Objects.equals(intervalDTO.getInterval(), maxInterval))
-                .collect(Collectors.toList()));
+                .filter(intervalDTO -> {
+                    LOG.info("Testing thread async (max)");
+                    return Objects.equals(intervalDTO.getInterval(), maxInterval);
+                }).collect(Collectors.toList()));
     }
 }
